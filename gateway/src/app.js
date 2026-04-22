b513fcd074b629d36d00ceb7bc71e6241d6ea482
hieusieucup <hieuspace1202@gamil.com>
2026-04-22 23:46:44 +0700

    fix bug log

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { requestIdMiddleware, accessLogMiddleware, notFoundHandler, errorHandler } = require('circlek-core');
+const { requestIdMiddleware, notFoundHandler, errorHandler } = require('circlek-core');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(requestIdMiddleware);
-app.use(accessLogMiddleware);
 app.use(routes);
 app.use(notFoundHandler);
 app.use(errorHandler);
